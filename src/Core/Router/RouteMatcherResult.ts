af6dd7e750620d5fff68640f314883e400b8c9fe
Aleksei Zolotykh <aleksei.zolotykh@syngenta.com>
2022-05-12 01:14:16 +0200

    chore: remove unwanted routes

--- src/Core/Router/RouteMatcherResult.ts
+++ src/Core/Router/RouteMatcherResult.ts
@@ -1,7 +1,7 @@
 import {Route} from "./Route";
 
 export class RouterMatcherResult {
-  constructor(public readonly routes: Route [] = []){}
+  constructor(private readonly routes: Route [] = []){}
 
   get isFound(): boolean{
     return !!this.routes.length;
