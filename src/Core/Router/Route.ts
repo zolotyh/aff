2178f7cbb3983a0ae9f288892f9c970f9c49513c
Aleksei Zolotykh <aleksei.zolotykh@syngenta.com>
2022-05-12 13:49:51 +0200

    chore: add linter after commit

--- src/Core/Router/Route.ts
+++ src/Core/Router/Route.ts
@@ -1,25 +1,25 @@
 import { routeTemplateToRegExp } from './routeTemplateToRegExp';
 
 export class Route {
-    static fromPath(path: string){
-        return new Route(path);
-    }
+  static fromPath(path: string) {
+    return new Route(path);
+  }
 
-    private readonly regExp: RegExp;
+  private readonly regExp: RegExp;
 
-    constructor(public readonly path: string) {
-        const cleanPath = Route.removeLastSlash(path);
-        this.regExp = new RegExp(routeTemplateToRegExp(cleanPath), 'g');
-    }
+  constructor(public readonly path: string) {
+    const cleanPath = Route.removeLastSlash(path);
+    this.regExp = new RegExp(routeTemplateToRegExp(cleanPath), 'g');
+  }
 
-    private static removeLastSlash(input: string): string{
-        if(input.endsWith('/')){
-            return input.replace(/\/$/, '');
-        }
-        return input;
+  private static removeLastSlash(input: string): string {
+    if (input.endsWith('/')) {
+      return input.replace(/\/$/, '');
     }
+    return input;
+  }
 
-    public match(routePath: string): RegExpMatchArray | null {
-        return this.regExp.exec(routePath);
-    }
+  public match(routePath: string): RegExpMatchArray | null {
+    return this.regExp.exec(routePath);
+  }
 }
